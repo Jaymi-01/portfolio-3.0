007ce8f92c2a953217fe6fcedf5a5ea69efe1454
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-26 16:10:40 +0100

    feat: set a global padding

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import Header from './components/Header'
+import About from './components/About'
 
 function App() {
 
   return (
     <>
       <Header />
+
+      <div className="mx-auto max-w-[1300px]">
+        <section id="about" className="scroll-mt-20 pt-20 pb-20">
+          <About />
+        </section>
+      </div>
     </>
   )
 }

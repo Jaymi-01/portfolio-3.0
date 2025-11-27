3c3d211df1cc1cd9a16f4a8557dc0265e31eec1c
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-11-27 10:41:23 +0100

    feat: added a constant file for all my data

---
+++ src/constant/Index.tsx
@@ -0,0 +1,86 @@
+import { FaArrowRight } from "react-icons/fa";
+
+export const proficiency = [
+  {
+    text: "Html",
+  },
+  {
+    text: "CSS",
+  },
+  {
+    text: "Javascript",
+  },
+  {
+    text: "TailwindCSS",
+  },
+  {
+    text: "ReactJs",
+  },
+  {
+    text: "ReactTs",
+  },
+  {
+    text: "React Native",
+  },
+  {
+    text: "C#",
+  },
+  {
+    text: "Firebase",
+  },
+  {
+    text: "Vue",
+  },
+  {
+    text: "git",
+  },
+  {
+    text: "TypeScript",
+  }
+];
+
+export const projects = [
+  {
+    title: "Afuni Ticket App",
+    tools: "React + TailwindCSS + JavaScript",
+    text: "A ticketing website built using CRUD operation, with data displayed in real time.",
+    link: "https://ticket-app-react.vercel.app/",
+    icon: <FaArrowRight />,
+  },
+  {
+    title: "Blogr Landing Page",
+    tools: "React + TailwindCSS + JavaScript",
+    text: "A landing page for a Virtual Reality build platform",
+    link: "https://blogr-for-dev.vercel.app/",
+    icon: <FaArrowRight />,
+  },
+  {
+    title: "Sunnyside Landing Page",
+    tools: "html + CSS + JavaScript",
+    text: "A landing page for a full-service creative agency specializing in helping brands grow fast.",
+    link: "https://the-sunnyside-landing-page.netlify.app/",
+    icon: <FaArrowRight />,
+  },
+  {
+    title: "Color Guessing Game",
+    tools: "html + CSS + JavaScript",
+    text: "A responsive and addictive color guessing game",
+    link: "https://joel-color-guessing-game.netlify.app/",
+    icon: <FaArrowRight />,
+  },
+  {
+    title: "Advice Generator App",
+    tools: "html + CSS + JavaScript",
+    text: "A webpage that uses the adviceslip Api to generate a new advice on every page load.",
+    link: "https://my-auto-advice-generator.netlify.app/",
+    icon: <FaArrowRight />,
+  },
+  // {
+  //   title: "Movie App",
+  //   tools: "React + TailwindCSS + JavaScript",
+  //   text: "A webpage built with TVShow API that contains several movie recommendation.",
+  //   link: "https://joel-movie-app.vercel.app/",
+  //   icon: <FaArrowRight />,
+  // },
+  
+];
